81636562072898b97799be2c99befdd544d99cef
gongyr <gongyr@LCBLAB2110-16.uccob.uc.edu>
2019-09-19 09:19:22 -0400

    First try

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -15,10 +15,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		for (int i = 1; i < lines; i++) {
+			for (int j = 1; j <lines - i; j++) {
+				System.out.print(" ");
 			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print(k);
+			}
+
 			System.out.println("");
 		}
 	}
