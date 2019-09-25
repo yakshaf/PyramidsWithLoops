2f6d4d73c53569fa22723996b6f93bf2ddcb1451
gongyr <gongyrf@gmail.com>
2019-09-25 16:48:25 -0400

    It works!

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -15,12 +15,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		int lines = 5;
-		for (int i = 1; i < lines; i++) {
-			for (int j = 1; j <lines - i; j++) {
+		for (int i = 1; i <= lines; i++) {
+			for (int j = 1; j <=lines - i; j++) {
 				System.out.print(" ");
 			}
-			for (int k = 1; k <= i; k++) {
-				System.out.print(k);
+			for (int j = 1; j <= i*2-1; j++) {
+				System.out.print(i); //
+			}
+			for (int j = 1; j <=lines; j++) {
+				System.out.print(" ");
 			}
 
 			System.out.println("");
